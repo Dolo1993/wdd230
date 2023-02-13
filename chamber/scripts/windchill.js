f15d2830101ac07dbc70137ad439a4d03ee5aaed
Emmanuel DK  Dolo <emmanueldkdolo1993@gmail.com>
2023-02-13 00:09:13 +0000

    that's good

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,20 +1,18 @@
 function calculateWindChill() {
-    // Get temperature and wind speed from input fields
-    var temperature = document.getElementById("temperature").value;
-    var windSpeed = document.getElementById("windSpeed").value;
-  
-    // Check if the input values meet the specification limits
-    if (temperature <= 50 && windSpeed > 3) {
-      // Calculate wind chill factor using the formula
-      var windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
-  
-      // Round the wind chill factor to 1 decimal place
-      windChill = windChill.toFixed(1);
-  
-      // Display the wind chill factor
-      document.getElementById("weather").innerHTML = "Wind Chill: " + windChill + "°F";
-    } else {
-      // Display "N/A" if the input values do not meet the requirements
-      document.getElementById("weather").innerHTML = "N/A";
-    }
+  var temperature = document.getElementById("temperature").textContent;
+  var windSpeed = document.getElementById("windspeed").textContent;
+
+  if (temperature <= 50 && windSpeed > 3.0) {
+    var windChillFactor = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+    document.getElementById("windchill").textContent = windChillFactor.toFixed(2) + "°F";
+  } else {
+    document.getElementById("windchill").textContent = "N/A";
   }
+}
+
+calculateWindChill();
+ 
+
+
+
+
