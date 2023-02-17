ad7509b12e0f1ccff4854ffd255c7a14a3313b1a
Emmanuel DK  Dolo <emmanueldkdolo1993@gmail.com>
2023-02-18 00:11:11 +0530

    loding complete

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,24 +1,38 @@
-const lazyImages = document.querySelectorAll('img[data-src');
+ // Select all images with the class 'lazy'
+const lazyImages = document.querySelectorAll('img[data-src]');
 
-const lazyLoad = (image) => {
-  image.src = image.dataset.src;
+// Create a function to load the image
+function loadLazyImage(image) {
+  // Get the image source from the 'data-src' attribute
+  const src = image.getAttribute('data-src');
+  if (!src) {
+    return;
+  }
+  
+  // Load the image and replace the 'src' attribute
+  image.src = src;
+  
+  // Remove the 'lazy' class
   image.classList.remove('lazy');
-};
+}
 
-const options = {
-  rootMargin: '0px',
-  threshold: 0.1
-};
-
-const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      lazyLoad(entry.target);
-      observer.unobserve(entry.target);
+// Create a function to handle the scroll event
+function handleScroll() {
+  // Loop through all the lazy images
+  lazyImages.forEach((image) => {
+    // Check if the image is in the viewport
+    const bounding = image.getBoundingClientRect();
+    if (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    ) {
+      // If the image is in the viewport, load it
+      loadLazyImage(image);
     }
   });
-}, options);
+}
 
-lazyImages.forEach((image) => {
-  observer.observe(image);
-});
+// Add an event listener for the 'scroll' event
+window.addEventListener('scroll', handleScroll);
