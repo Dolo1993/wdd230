b42a00f187afd2d36372130181fd563f793eb47a
Emmanuel DK  Dolo <emmanueldkdolo1993@gmail.com>
2023-02-19 18:59:14 +0530

    updated lazy loading

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,46 +1,30 @@
- // Select all images with the class 'lazy'
-const lazyImages = document.querySelectorAll('[data-src]');
+const lazyImages = document.querySelectorAll('img.lazy');
 
-// Create a function to load the image
-function loadLazyImage(image) {
-  // Get the image source from the 'data-src' attribute
-  const src = image.getAttribute('data-src');
-  if (!src) {
-    return;
-  }
-  
-  // Load the image and replace the 'src' attribute
-  image.src = src;
-  
-  // Remove the 'lazy' class
-  image.classList.remove('lazy');
-}
+const lazyLoad = target => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        const src = img.getAttribute('data-src');
 
-// Create a function to handle the scroll event
-function handleScroll() {
-  // Loop through all the lazy images
-  lazyImages.forEach((image) => {
-    // Check if the image is in the viewport
-    const bounding = image.getBoundingClientRect();
-    if (
-      bounding.top >= 0 &&
-      bounding.left >= 0 &&
-      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    ) {
-      // If the image is in the viewport, load it
-      loadLazyImage(image);
-    }
+        img.setAttribute('src', src);
+        img.classList.remove('lazy');
+
+        observer.disconnect();
+      }
+    });
   });
-}
 
-// Add an event listener for the 'scroll' event
-window.addEventListener('scroll', handleScroll); 
+  io.observe(target);
+};
 
+lazyImages.forEach(lazyLoad);
 // initialize display elements
 const todayDisplay = document.querySelector("#today");
 const visitsDisplay = document.querySelector("#visits");
-const daysLeftOutput = document.querySelector("#daysleft");
+const daysLeftOutput = document.querySelector("#daysleft"); 
+
+
 
 // get the stored value in localStorage
 let numVisits = Number(window.localStorage.getItem("visits-ls")); // Using the Number() function ensures that if the storage item does not exist, it will be converted into a zero (0) which helps on the if block condition.
