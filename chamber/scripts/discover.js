596194855b60f3e492ed63bb99cfb03bc59f084e
Emmanuel DK  Dolo <emmanueldkdolo1993@gmail.com>
2023-02-21 17:30:34 +0000

    added current time visit

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,6 +1,6 @@
 const imgElements = document.querySelectorAll("img[data-src]"); 
 const lazyLoadingObserver=new IntersectionObserver(
-  (entries, observer)=>{
+  (entries, observer)=>{ 
     console.log(entries);  
     entries.forEach((entry) => { 
     if (!entry.isIntersecting)return; 
@@ -15,21 +15,6 @@ const lazyLoadingObserver=new IntersectionObserver(
 imgElements.forEach((img) =>lazyLoadingObserver.observe(img));
 
  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // initialize display elements
 const todayDisplay = document.querySelector("#today");
 const visitsDisplay = document.querySelector("#visits");
@@ -37,60 +22,23 @@ const daysLeftOutput = document.querySelector("#daysleft");
 
 
 
-// get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("visits-ls")); // Using the Number() function ensures that if the storage item does not exist, it will be converted into a zero (0) which helps on the if block condition.
+ // Get the current date
+ const currentDate = new Date();
 
-// determine if this is the first visit or display the number of visits.
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `This is your first visit!`;
-}
-
-// increment the number of visits.
-numVisits++;
-// store the new number of visits value
-localStorage.setItem("visits-ls", numVisits);
-// show todays date demonstration
-todayDisplay.textContent = Date.now();
-//84600000 equals the number of miliseconds in one day.
-
-// Days until Christmas
-let today = new Date();
-let christmas = new Date(today.getFullYear(), 11, 25);
-// check if is the waing days of December, if so ... change to next year.
-if (today.getMonth() == 11 && today.getDate() > 25) {
-	christmas.setFullYear(christmas.getFullYear() + 1);
-}
-
-let daysleft = (christmas.getTime() - Date.now()) / 84600000;
-
-daysLeftOutput.innerHTML = `${daysleft.toFixed(
-	0
-)} days<br> 🎄Christmas is ${christmas}`;
+ // Get the last visit date from local storage
+ const lastVisit = localStorage.getItem('lastVisit');
  
-
-
-
-
-
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'}; 
-document.getElementById('currentdate').textContent=new Date().toLocaleDateString('en-Us', options); 
-
-
-// select the elements to manipulate (output to)
-const datefield = document.querySelector(".date");
-const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
-
-// derive the current date using a date object
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-now
-);
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-dateStyle: "full"
-}).format(now);
-// long, medium, short options ... try them
-
-datefield.innerHTML = `<em>${fulldate}</em>`;
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+ // If there is no last visit date in local storage, set it to the current date
+ if (!lastVisit) {
+   localStorage.setItem('lastVisit', currentDate);
+ } else {
+   // Calculate the number of days since the last visit
+   const daysSinceVisit = Math.round((currentDate - new Date(lastVisit)) / (1000 * 60 * 60 * 24));
+   
+   // Display the number of days on the page
+   const daysSinceVisitElement = document.getElementById('days-since-visit');
+   daysSinceVisitElement.textContent = daysSinceVisit;
+   
+   // Update the last visit date in local storage to the current date
+   localStorage.setItem('lastVisit', currentDate);
+ }
